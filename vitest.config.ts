48ca78bbbbe8459021f750c4de6dc299f4a581f9
aantthony <277425+aantthony@users.noreply.github.com>
2026-09-12 12:04:22 +1000

    Update dependencies and resolve security audit findings (#92)

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['lib/**/*.test.ts', 'worker/**/*.test.ts'],
+    benchmark: {
+      include: ['lib/**/*.bench.ts'],
+    },
   },
 });
